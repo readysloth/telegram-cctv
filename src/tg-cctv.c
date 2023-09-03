3af67e5a5477c4ff75f1cd0e7d40c1ed434bc0e3
readysloth <readysloth@protonmail.com>
2023-09-03 22:21:17 +0300

    feat(src/tg-cctv.c): Added disabling of USB hub for energy efficiency

--- src/tg-cctv.c
+++ src/tg-cctv.c
@@ -12,42 +12,83 @@
 
 #define IMAGE_BUFFER_SIZE WIDTH*HEIGHT*3
 
+#define USB_HUB_DEFAULT "1-1"
+#define USB_HUB "1-1"
+
+#define WARNING_MSG "Default USB hub is " USB_HUB_DEFAULT \
+                    " and specified USB hub is " USB_HUB "." \
+                    " Perhaps you want to redefine it, if default matches specified."
+
+#pragma message WARNING_MSG
+#warning See the message above
+
 uint8_t RAW_IMAGE_BUFFER[IMAGE_BUFFER_SIZE] = {0};
+
+struct device_info {
+  int fd;
+  void *fmt;
+};
+
+
+struct device_info prepare_video_device(char *video_device,
+                                        unsigned int *actual_width,
+                                        unsigned int *actual_height){
+  struct device_info info = {0};
+  info.fd = open_device(video_device);
+  if (info.fd < 0){
+    log_info("Device open error");
+    goto error;
+  }
+
+  info.fmt = device_setup(info.fd, actual_width, actual_height);
+  if (!info.fmt){
+    goto error;
+  }
+
+  return info;
+error:
+  if (info.fd > 0){
+    close_device(info.fd);
+  }
+  info.fd = -1;
+  info.fmt = NULL;
+  return info;
+}
+
 
 int main(int argc, char *argv[]){
   int ret = 0;
-  int device_fd = -1;
   unsigned int actual_width = 0;
   unsigned int actual_height = 0;
   FILE *raw_img_file = NULL;
 
   size_t png_size = 0;
   uint8_t *png_img_buf = NULL;
-
-  void *fmt_info = NULL;
-
+  struct device_info info = {0};
   char *video_device = argc < 2 ? "/dev/video0" : argv[1];
+  bool disable_usb = argc < 3 ? false : !strcmp(argv[2], "disable_usb");;
 
   log_set_level(LOG_INFO);
   log_info("Started");
-  device_fd = open_device(video_device);
-  if (device_fd < 0){
-    log_info("Device open error, exiting");
-    goto error;
-  }
-
-  fmt_info = device_setup(device_fd, &actual_width, &actual_height);
-  if (!fmt_info){
-    goto error;
-  }
+  log_info(disable_usb ? "ALL USB devices will be disabled in every iteration" : "As usual");
 
   raw_img_file = fmemopen(RAW_IMAGE_BUFFER, sizeof(RAW_IMAGE_BUFFER), "wb");
   if (!raw_img_file){
     goto error;
   }
 
-  while(true) {
-    get_frame(raw_img_file, device_fd, fmt_info);
+  while(true){
+    if (info.fd == 0){
+      info = prepare_video_device(video_device,
+                                  &actual_width,
+                                  &actual_height);
+      if (info.fd == -1){
+        log_error("Cannot prepare video device");
+        continue;
+      }
+    }
+
+    get_frame(raw_img_file, info.fd, info.fmt);
 
     unsigned int err = lodepng_encode24(&png_img_buf,
                                         &png_size,
@@ -61,7 +102,17 @@ int main(int argc, char *argv[]){
 
     upload_buffer(png_img_buf, png_size, TG_BOT_SEND_PHOTO_ENDPOINT);
     rewind(raw_img_file);
-    sleep(2*60);
+    if (disable_usb){
+      close_device(info.fd);
+      info.fd = 0;
+      info.fmt = NULL;
+      system("echo " USB_HUB " > /sys/bus/usb/drivers/usb/unbind");
+    }
+    sleep(3*60);
+    if (disable_usb){
+      system("echo " USB_HUB " > /sys/bus/usb/drivers/usb/bind");
+      sleep(5);
+    }
   }
 
   goto cleanup;
@@ -69,8 +120,8 @@ int main(int argc, char *argv[]){
 error:
   ret = 1;
 cleanup:
-  if (device_fd > 0){
-    close_device(device_fd);
+  if (info.fd > 0){
+    close_device(info.fd);
   }
   if (raw_img_file){
     fclose(raw_img_file);
